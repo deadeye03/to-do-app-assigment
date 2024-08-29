1f5ef70445deaae164ee4556eb4f3970695bc4ef
deadeye03 <yakshitkr@gmail.com>
2024-08-29 14:07:26 +0530

    initial commit

--- app/page.js
+++ app/page.js
@@ -1,25 +1,16 @@
-"use client"
-import { fetchAllTask } from "@/actions/taskAction";
-import { useState,useEffect } from "react";
+
 import Alltask from "@/components/Alltask";
 import AllTaskMobile from "@/components/AllTaskMobile";
 
 export default function Home() {
-  const [allTask, setAllTask] = useState([])
-  const getAllTask = async(page) => {
-    const tasks=await fetchAllTask(page)
-    setAllTask(tasks.allTask)
-  }
-  useEffect(() => {
-    getAllTask();
-  }, [])
+
   return (
     <>
       <div className="sm:hidden">
       <Alltask/>
       </div>
       <div className="md:hidden lg:hidden xl:hidden">
-        <AllTaskMobile allTask={allTask}/>
+        <AllTaskMobile />
       </div>
     </>
   );
